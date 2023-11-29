857c2fb269b2faa0f3571893e65b67edb352ce63
Lars <laha0006@stud.kea.dk>
2023-11-29 09:40:24 +0100

    refactor to LocalDate birthday type

--- src/main/java/datasource/FileHandler.java
+++ src/main/java/datasource/FileHandler.java
@@ -42,35 +42,21 @@ public class FileHandler {
             switch (memberType){
                 case "Passive" ->
                     addMember = new PassiveMember(attributes[1],
-                            attributes[2],
+                            LocalDate.parse(attributes[2]),
                             attributes[3],
                             attributes[4],
                             Double.parseDouble(attributes[5].trim()));
 
                 case "Exercise" ->
                     addMember = new ExerciseMember(attributes[1],
-                            attributes[2],
+                            LocalDate.parse(attributes[2]),
                             attributes[3],
                             attributes[4],
                             Double.parseDouble(attributes[5].trim()));
 
                 case "Competitive" ->
                     addMember = new CompetitiveMember(attributes[1],
-                            attributes[2],
-                            attributes[3],
-                            attributes[4],
-                            Double.parseDouble(attributes[5].trim()));
-
-                case "Junior" ->
-                    addMember = new JuniorMember(attributes[1],
-                            attributes[2],
-                            attributes[3],
-                            attributes[4],
-                            Double.parseDouble(attributes[5].trim()));
-
-                case "Senior" ->
-                    addMember = new SeniorMember(attributes[1],
-                            attributes[2],
+                            LocalDate.parse(attributes[2]),
                             attributes[3],
                             attributes[4],
                             Double.parseDouble(attributes[5].trim()));
