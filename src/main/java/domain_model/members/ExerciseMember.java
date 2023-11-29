41379e488dc5c090f921c36aba0e3a1dd1bbb7e7
Lars <laha0006@stud.kea.dk>
2023-11-29 09:40:17 +0100

    refactor to LocalDate birthday type

--- src/main/java/domain_model/members/ExerciseMember.java
+++ src/main/java/domain_model/members/ExerciseMember.java
@@ -1,8 +1,10 @@
 package domain_model.members;
 
+import java.time.LocalDate;
+
 public class ExerciseMember extends Member {
     private String type = "Exercise";
-    public ExerciseMember(String name, String birthDate, String email, String discipline, double subscriptionValue) {
+    public ExerciseMember(String name, LocalDate birthDate, String email, String discipline, double subscriptionValue) {
         super(name, birthDate, email, discipline, subscriptionValue);
     }
 
