e7563a369134b0e1644d47c2325674e00f188f43
chye0001 <chye0001@stud.kea.dk>
2023-11-29 11:36:11 +0100

    added: converted datatype for Type from string to enum

--- src/main/java/domain_model/members/ExerciseMember.java
+++ src/main/java/domain_model/members/ExerciseMember.java
@@ -1,15 +1,17 @@
 package domain_model.members;
 
+import domain_model.MemberType;
+
 import java.time.LocalDate;
 
 public class ExerciseMember extends Member {
-    private String type = "Exercise";
+    private MemberType type = MemberType.EXERCISE;
     public ExerciseMember(String name, LocalDate birthDate, String email, String discipline, double subscriptionValue) {
         super(name, birthDate, email, discipline, subscriptionValue);
     }
 
     @Override
-    public String getType() {
+    public MemberType getType() {
         return type;
     }
 }
