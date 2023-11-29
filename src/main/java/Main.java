86c5c82e53da3910e272eb7d31e23148bdee3662
Lars <laha0006@stud.kea.dk>
2023-11-29 09:41:02 +0100

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import jdk.jfr.Timespan;
 import userinterface.Userinterface;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-
         Userinterface ui = new Userinterface();
         ui.startProgram();
     }
