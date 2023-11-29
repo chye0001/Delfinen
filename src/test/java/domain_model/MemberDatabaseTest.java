4eeb5855d83cfea4f51192b7a626af968d576589
chye0001 <chye0001@stud.kea.dk>
2023-11-29 13:11:50 +0100

    added: unittest passed 5 of 5

--- src/test/java/domain_model/MemberDatabaseTest.java
+++ src/test/java/domain_model/MemberDatabaseTest.java
@@ -20,6 +20,8 @@ class MemberDatabaseTest {
 
     @Test
     public void testLoadDatabase(){
+
+        memberDatabase.addMemberToList(MemberType.EXERCISE, "Jane Test", LocalDate.parse("1999-09-03"), "test@email.dk", "Crawl", 299.99);
         memberDatabase.loadMemberDatabase();
 
         MemberType type = memberDatabase.getClubMembers().get(0).getType();
@@ -87,5 +89,31 @@ class MemberDatabaseTest {
         assertEquals(expectedString,memberDatabase.showListOfMembers());
     }
 
+    @Test
+    void show_One_Year_Income_Forecast(){
+        memberDatabase.addMemberToList(MemberType.PASSIVE, "Test", LocalDate.parse("2001-02-23"), "test@test.dk", "none", 100.50);
+        memberDatabase.addMemberToList(MemberType.EXERCISE, "Test2", LocalDate.parse("2002-02-23"), "test2@test2.dk", "none", 200.40);
 
+        double expectedOneYearIncome = memberDatabase.getClubMembers().get(0).getSubscriptionCost() +
+                memberDatabase.getClubMembers().get(1).getSubscriptionCost();
+        double actualOneYearIncome = memberDatabase.showIncomeForecast();
+
+        assertEquals(expectedOneYearIncome, actualOneYearIncome);
+    }
+
+    @Test
+    void showListOfSubscription() {
+        MemberType type = MemberType.PASSIVE;
+        String name = "PersonTest";
+        LocalDate birthDate = LocalDate.parse("1999-09-03");
+        String email = "test@email.dk";
+        String discipline = "Crawl";
+        double subscriptionCost = 299.99;
+
+        memberDatabase.addMemberToList(type , name, birthDate, email, discipline, subscriptionCost);
+        String expectedSubscriptionList = "Name: PersonTest / Age: 24 / Activity type: PASSIVE / Subscription: Subscription: , Price: 299.99, Last payment date: 2023-11-29, Next payment date: 2024-11-29, Payment status: Paid, Debt: 0.0\n";
+        String actualSubscriptionList = memberDatabase.showListOfSubscription();
+
+        assertEquals(expectedSubscriptionList, actualSubscriptionList);
+    }
 }
